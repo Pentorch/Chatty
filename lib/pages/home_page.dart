7fd50ac4ab8b2c137f38c0e045b27ae6e8250eed
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-27 15:00:58 +0700

    styling widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,36 @@
+import 'package:chatty/thema.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
 Widget build(BuildContext context) {
     return Scaffold(
-      body : Image.asset('assets/images/profile.png'),
-     
+      backgroundColor: blueColor,
+      body : SafeArea(
+        child: Center(
+        child: Column(
+        children: [
+        SizedBox(height: 40),
+        Image.asset('assets/images/profile.png',
+          width: 100,
+          height: 100,
+        ),
+        SizedBox(height: 20),
+        Text('Willy Agustino Efendi',
+        style: TextStyle(
+          fontSize: 20,
+          color: whiteColor,
+        ),
+        ),
+        SizedBox(height: 2),
+        Text('Travel Freelancer',
+        style: TextStyle(
+          fontSize: 16,
+          color: lightBlueColor,
+        ),
+        ),
+      ],),),
+      )
     );
   }
 }
--- lib/thema.dart
+++ lib/thema.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-Color blueColor = Color(0x1F8DF5);
+Color blueColor = Color(0xff1F8DF5);
 Color whiteColor = Color(0xffffffff);
-Color lightBlueColor = Color(0x9ED0FF);
+Color lightBlueColor = Color(0xff9ED0FF);
 Color blackColor = Color(0xfF000000);
 Color greyColor = Color(0x808BA2);
 Color greenColor = Color(0x29CB9E);
