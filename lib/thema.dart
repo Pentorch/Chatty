8c7298eb226e087dd94ec7a8f1d4b4e2d1148d26
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-27 15:42:03 +0700

    styling components

--- lib/thema.dart
+++ lib/thema.dart
@@ -4,8 +4,8 @@ Color blueColor = Color(0xff1F8DF5);
 Color whiteColor = Color(0xffffffff);
 Color lightBlueColor = Color(0xff9ED0FF);
 Color blackColor = Color(0xfF000000);
-Color greyColor = Color(0x808BA2);
-Color greenColor = Color(0x29CB9E);
+Color greyColor = Color(0xff808BA2);
+Color greenColor = Color(0xff29CB9E);
 
 TextStyle titleTextStyle = TextStyle(
   color: blackColor,
@@ -14,6 +14,6 @@ TextStyle titleTextStyle = TextStyle(
 );
 
 TextStyle subtitleTextStyle = TextStyle(
-  color: greenColor,
+  color: greyColor,
   fontWeight: FontWeight.w300,
 );
