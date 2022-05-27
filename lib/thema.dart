d7beb210d6e8729a8a673a7805a28f5b8ddfbfa1
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-27 11:59:44 +0700

    thema

---
+++ lib/thema.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = Color(0x1F8DF5);
+Color whiteColor = Color(0xffffffff);
+Color lightBlueColor = Color(0x9ED0FF);
+Color blackColor = Color(0xfF000000);
+Color greyColor = Color(0x808BA2);
+Color greenColor = Color(0x29CB9E);
+
+TextStyle titleTextStyle = TextStyle(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+
+TextStyle subtitleTextStyle = TextStyle(
+  color: greenColor,
+  fontWeight: FontWeight.w300,
+);
