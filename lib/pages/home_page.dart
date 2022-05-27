92a6ba9ff39f62dc79933b0cbb3b04a14dc63016
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-27 11:26:47 +0700

    assets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,12 +4,8 @@ class HomePage extends StatelessWidget {
   @override
 Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        'Home Page',
-        style: TextStyle(
-          fontSize: 40,
-        ),
-      ),
+      body : Image.asset('assets/images/profile.png'),
+     
     );
   }
 }
